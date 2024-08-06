a96321152c90874ad642ef1cd7ac33e1dbb5ed65
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-06 19:56:00 +0300

    Errorfix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,7 @@ import random
 
 
 async def async_generator() -> AsyncIterator[float]:
+    """Async Generator that yields random numbers"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(1, 10)
+        yield random.uniform(0, 10)
