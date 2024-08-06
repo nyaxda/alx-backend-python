67b1f3e125b68f7fc5502fd072a5ec1aff380484
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-06 19:58:18 +0300

    Refactor async_generator to use random.random() instead of random.uniform()

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator() -> AsyncIterator[float]:
     """Async Generator that yields random numbers"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random * 10
