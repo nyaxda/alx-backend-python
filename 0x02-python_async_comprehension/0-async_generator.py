c616d943270141213101767cba23cd9a0c84c27a
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-06 20:09:20 +0300

    Refactor async_generator to use AsyncIterator instead of AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Async Generators Module"""
 
-from typing import AsyncGenerator
+from typing import AsyncIterator
 import asyncio
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncIterator[float]:
     """Async Generator that yields random numbers"""
     for _ in range(10):
         await asyncio.sleep(1)
