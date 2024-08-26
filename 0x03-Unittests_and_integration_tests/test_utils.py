1aa8ef24971b81665b0da61c2540d8d6c7aacb96
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-26 08:44:58 -0700

    Added test_utils file

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Utils test"""
+
+from parameterized import parameterized
+from unittest import TestCase
+
+
+class TestAccessNestedMap(TestCase):
+    @parameterized.expand([
+         ({"a": 1}, ("a",), 1),
+         ({"a": {"b": 2}}, ("a",), {"b": 2}),
+         ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def text_access_nested_map(self, nested_map, path, expected):
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
