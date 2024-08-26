a9ae38baf8972618d8cab85bc9904379aaf22ad4
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-26 08:52:32 -0700

    Refactor test_access_nested_map function name in test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,10 +2,13 @@
 """ Utils test"""
 
 from parameterized import parameterized
-from unittest import TestCase
+import unittest
+
+access_nested_map = __import__('utils').access_nested_map
 
 
-class TestAccessNestedMap(TestCase):
+class TestAccessNestedMap(unittest.TestCase):
+    """TestAccessNestedMap class"""
     @parameterized.expand([
          ({"a": 1}, ("a",), 1),
          ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -14,3 +17,6 @@ class TestAccessNestedMap(TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
+
+if __name__ == "__main__":
+    unittest.main()
