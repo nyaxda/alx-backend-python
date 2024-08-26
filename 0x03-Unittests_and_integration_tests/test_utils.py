2637c4620a29a15b119fa5173f9d27b56e9d29fe
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-26 09:44:33 -0700

    Refactor test_access_nested_map function name in test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,8 +3,10 @@
 
 from parameterized import parameterized
 import unittest
+from unittest.mock import patch
 
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,6 +27,19 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path, exception):
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """tests https requests"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload", False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, test_get):
+        test_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
