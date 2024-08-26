eb11e067c1a402ac31ae06b0a5df452f8271e603
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-26 09:58:47 -0700

    Refactor memoize decorator in test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,7 @@ from unittest.mock import patch
 
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -40,6 +41,25 @@ class TestGetJson(unittest.TestCase):
         test_get.return_value.json.return_value = test_payload
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+    """does memoization tests"""
+    def test_memoize(self):
+        """test memoize"""
+        class TestClass:
+            """test class"""
+            def a_method(self):
+                """a method"""
+                return 42
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method', return_value=42) as mock:
+            tc = TestClass()
+            # calling twice to ensure it was called only once
+            self.assertEqual(tc.a_property, 42)
+            self.assertEqual(tc.a_property, 42)
+            mock.assert_called_once()
 
 
 if __name__ == "__main__":
